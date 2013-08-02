78f8148252fa628ceed6d5de63b780e635e4c817
DAMason <DAMason@gmail.com>
2013-08-02 13:54:09 -0500

    Now with Inheritance!

---
+++ Crap.h
@@ -0,0 +1,174 @@
+// -*- C++ -*-
+//
+// Package:    SusyNtuplizer
+// Class:      Crap.h
+// 
+/*
+
+ Description: an analyzer for susy::Event
+
+ Implementation:
+
+*/
+//
+// Original Author:  Dongwook Jang
+// $Id: Crap.h,v 1.8 2013/05/05 12:01:32 yiiyama Exp $
+//
+
+#ifndef Crap_h
+#define Crap_h
+
+#include <TChain.h>
+#include <TString.h>
+#include <TPRegexp.h>
+#include <TArrayI.h>
+
+#include <map>
+#include <set>
+#include <fstream>
+
+#include <SusyEvent.h>
+
+class Crap {
+public:
+  Crap(TTree&);
+  virtual ~Crap();
+
+  /* Main analyzer function to be defined */
+  //virtual void Run();
+
+  /* parameter configuration functions */
+  void IncludeAJson(TString const&);
+  void SetOutput(TString const& v) { outputName = v; }
+  void SetLogFile(TString const& v) { logFileName = v;}
+  void SetPrintInterval(int v) { printInterval = v; }
+  void SetPrintLevel(int v) { printLevel = v; }
+  void SetProcessNEvents(int v) { processNEvents = v; }
+  void AddHltName(TString const& v) { hltNames.push_back(v + "_v*"); }
+  void CopyEvents(bool v) { copyEvents = v; }
+
+protected:
+  bool IsGoodLumi(UInt_t, UInt_t) const;
+  bool PassTriggers() const;
+
+  /* container of all event data */
+  susy::Event event;
+  /* input tree */
+  TTree *fTree;
+  /* suffix of the output file */
+  TString outputName;
+  /* log file name */
+  TString logFileName;
+  /* verbosity - 0 => no printout, 1 => print function control flow, 2 => print event processing flow, 3 => print event dump */
+  int printLevel;
+  /* print frequency */
+  unsigned printInterval;
+  /* maximum number of events */
+  int processNEvents;
+  /* HLT path names */
+  std::vector<TString> hltNames;
+  /* switch for saving skims */
+  bool copyEvents;
+  /* good lumi list */
+  std::map<unsigned, std::set<unsigned> > goodLumiList;
+  mutable std::pair<unsigned, unsigned> currentLumi;
+  mutable bool currentLumiIsGood;
+};
+
+Crap::Crap(TTree& tree) :
+  event(),
+  fTree(&tree),
+  outputName("analysis"),
+  logFileName(outputName + ".log"),
+  printLevel(0),
+  printInterval(1000),
+  processNEvents(-1),
+  hltNames(),
+  copyEvents(false),
+  goodLumiList(),
+  currentLumi(0, 0),
+  currentLumiIsGood(true)
+{
+  event.setInput(tree);
+}
+
+Crap::~Crap()
+{
+}
+
+void
+Crap::IncludeAJson(TString const& _fileName)
+{
+  if(_fileName == "") return;
+
+  std::ifstream inputFile(_fileName);
+  if(!inputFile.is_open()){
+    std::cerr << "Cannot open JSON file " << _fileName << std::endl;
+    return;
+  }
+
+  std::string line;
+  TString jsonText;
+  while(true){
+    std::getline(inputFile, line);
+    if(!inputFile.good()) break;
+    jsonText += line;
+  }
+  inputFile.close();
+
+  TPRegexp runBlockPat("\"([0-9]+)\":[ ]*\\[((?:\\[[0-9]+,[ ]*[0-9]+\\](?:,[ ]*|))+)\\]");
+  TPRegexp lumiBlockPat("\\[([0-9]+),[ ]*([0-9]+)\\]");
+
+  TArrayI positions(2);
+  positions[1] = 0;
+  while(runBlockPat.Match(jsonText, "g", positions[1], 10, &positions) == 3){
+    TString runBlock(jsonText(positions[0], positions[1] - positions[0]));
+    TString lumiPart(jsonText(positions[4], positions[5] - positions[4]));
+
+    unsigned run(TString(jsonText(positions[2], positions[3] - positions[2])).Atoi());
+    std::set<unsigned>& lumis(goodLumiList[run]);
+
+    TArrayI lumiPos(2);
+    lumiPos[1] = 0;
+    while(lumiBlockPat.Match(lumiPart, "g", lumiPos[1], 10, &lumiPos) == 3){
+      TString lumiBlock(lumiPart(lumiPos[0], lumiPos[1] - lumiPos[0]));
+      int begin(TString(lumiPart(lumiPos[2], lumiPos[3] - lumiPos[2])).Atoi());
+      int end(TString(lumiPart(lumiPos[4], lumiPos[5] - lumiPos[4])).Atoi());
+      for(int lumi(begin); lumi <= end; ++lumi)
+        lumis.insert(lumi);
+    }
+  }
+}
+
+bool
+Crap::IsGoodLumi(UInt_t run, UInt_t lumi) const
+{
+  if(goodLumiList.size() == 0) return true;
+  if(run == currentLumi.first && lumi == currentLumi.second) return currentLumiIsGood;
+  currentLumi.first = run;
+  currentLumi.second = lumi;
+  currentLumiIsGood = false;
+
+  std::map<unsigned, std::set<unsigned> >::const_iterator rItr(goodLumiList.find(run));
+  if(rItr != goodLumiList.end()){
+    std::set<unsigned>::const_iterator lItr(rItr->second.find(lumi));
+    if(lItr != rItr->second.end()) currentLumiIsGood = true;
+  }
+
+  return currentLumiIsGood;
+}
+
+bool
+Crap::PassTriggers() const
+{
+  unsigned nT(hltNames.size());
+  if(nT == 0) return true;
+
+  for(unsigned iT(0); iT != nT; ++iT)
+    if(event.hltMap.pass(hltNames[iT])) return true;
+
+  return false;
+}
+
+#endif
+
--- PhoAnalysis/CrapPhoAnalysis.cc
+++ PhoAnalysis/CrapPhoAnalysis.cc
@@ -44,87 +44,15 @@
 
 #endif //CMSSWENVIRONMENT
 
-#include "CrapPhoAnalysis.h"
+#include "../Crap.h"
+#include "../UtilPieces.h"
 
-template<typename T>
-bool
-PtGreater(const T* p1, const T* p2) {
-  return p1->momentum.Pt() > p2->momentum.Pt();
-}
+// Inherit from Crap
+class CrapPhoAnalysis: public Crap {
+  /* Main analyzer function to be defined */
+  virtual void Run();
+};
 
-template<typename T1, typename T2>
-bool
-isSameObject(const T1& p1, const T2& p2, double dRCut = 0.1)
-{
-  float dEta = p1.momentum.Eta() - p2.momentum.Eta();
-  float dPhi = TVector2::Phi_mpi_pi(p1.momentum.Phi() - p2.momentum.Phi());
-  return dEta*dEta + dPhi*dPhi < dRCut * dRCut;
-}
-
-void
-photonEffectiveAreas(double _eta, double* _effA)
-{
-  double& effACH(_effA[0]);
-  double& effANH(_effA[1]);
-  double& effAPh(_effA[2]);
-
-  // Source: CutBasedPhotonID2012 twiki
-  if(_eta < 1.){
-    effACH = 0.012;
-    effANH = 0.03;
-    effAPh = 0.148;
-  }
-  else if(_eta < 1.479){
-    effACH = 0.010;
-    effANH = 0.057;
-    effAPh = 0.13;
-  }
-  else if(_eta < 2.){
-    effACH = 0.014;
-    effANH = 0.039;
-    effAPh = 0.112;
-  }
-  else if(_eta < 2.2){
-    effACH = 0.012;
-    effANH = 0.015;
-    effAPh = 0.216;
-  }
-  else if(_eta < 2.3){
-    effACH = 0.016;
-    effANH = 0.024;
-    effAPh = 0.262;
-  }
-  else if(_eta < 2.4){
-    effACH = 0.02;
-    effANH = 0.039;
-    effAPh = 0.26;
-  }
-  else{
-    effACH = 0.012;
-    effANH = 0.072;
-    effAPh = 0.266;
-  }
-}
-
-void
-electronEffectiveAreas(double _eta, double &_effA)
-{
-  // Source: EgammaEARhoCorrection twiki
-  if(_eta < 1.)
-    _effA = 0.13;
-  else if(_eta < 1.479)
-    _effA = 0.14;
-  else if(_eta < 2.)
-    _effA = 0.07;
-  else if(_eta < 2.2)
-    _effA = 0.09;
-  else if(_eta < 2.3)
-    _effA = 0.11;
-  else if(_eta < 2.4)
-    _effA = 0.11;
-  else
-    _effA = 0.14;
-}
 
 ////////// MAIN ANALYSIS FUNCTION //////////
 void
